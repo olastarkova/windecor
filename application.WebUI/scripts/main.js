4689433fc1cf5648ec23e857dba3882cb1771ed9
olastarkova <olyakrk@gmail.com>
2016-10-23 02:06:52 +0300

    Ola - finishe sideBar

---
+++ application.WebUI/scripts/main.js
@@ -0,0 +1,12 @@
+﻿
+   $(document).ready(function() {
+        $(".sideBar .glyphicon").click(function () {
+            if ($(this).attr("aria-expanded") == "true") {
+                $(this).removeClass("glyphicon-minus").addClass("glyphicon-plus");
+            } else {
+                $(this).removeClass("glyphicon-plus").addClass("glyphicon-minus");
+            }
+
+           
+        });
+    });
